d56dbac7c8e2826c090b53f2074d4c38834214ef
Ngoc Tran <ngoc.tran@softwareone.com>
2025-07-06 16:05:27 +0000

    test mlflow 2

--- lambda/trigger_preprocessing/lambda_function.py
+++ lambda/trigger_preprocessing/lambda_function.py
@@ -10,9 +10,9 @@ def lambda_handler(event, context):
     print("🟢 Lambda triggered.")
     print("📨 Incoming event:", event)
 
-    # print("🧪 ENV DUMP:")
-    # for k, v in os.environ.items():
-    #     print(f"{k} = {v}")
+    print("🧪 ENV DUMP:")
+    for k, v in os.environ.items():
+        print(f"{k} = {v}")
 
 
     try:
