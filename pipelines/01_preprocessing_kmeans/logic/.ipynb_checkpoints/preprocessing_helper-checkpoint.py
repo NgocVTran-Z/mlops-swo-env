eed0eb4892ebcaa0b77c8bf761ec63018265874b
Ngoc Tran <ngoc.tran@softwareone.com>
2025-07-07 03:42:55 +0000

    test02

--- pipelines/01_preprocessing_kmeans/logic/.ipynb_checkpoints/preprocessing_helper-checkpoint.py
+++ pipelines/01_preprocessing_kmeans/logic/.ipynb_checkpoints/preprocessing_helper-checkpoint.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 import mlflow
-
+print(mlflow.__version__)
 
 # Add shared module to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../shared")))
